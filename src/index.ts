f9b9200e84c4722345c51b36a1d153eb4d7028c0
969939390@qq.com <969939390@qq.com>
2022-10-29 20:21:18 +0800

    feta:第二节课内容提交

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 
 import SButton from "./button";
 import SFCButton from "./SFCButton.vue";
+import JSXButton from "./JSXButton";
 
-createApp(SFCButton).mount("#app");
+createApp(JSXButton).mount("#app");
